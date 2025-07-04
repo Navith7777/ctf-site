606d900ffa4589126cb16e11f37470e537d1be4a
Your Name <you@example.com>
2025-07-04 19:01:20 +0530

    Your message

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,3 +1,4 @@
+import { FaDiscord, FaHeadset, FaPaperPlane } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -6,6 +7,7 @@ const Footer = () => {
         {/* Our Community */}
         <div className="bg-blue-900 rounded-xl p-6">
           <div className="flex items-center gap-2 text-lg font-semibold mb-2">
+          <FaDiscord className="text-2xl" />
             <span>Our Community</span>
           </div>
           <h3 className="text-xl font-semibold mb-2">Do not skip any beat.</h3>
@@ -20,6 +22,7 @@ const Footer = () => {
         {/* Customer Support */}
         <div className="bg-blue-900 rounded-xl p-6">
           <div className="flex items-center gap-2 text-lg font-semibold mb-2">
+          <FaHeadset className="text-2xl" />
             <span>Customer Support</span>
           </div>
           <h3 className="text-xl font-semibold mb-2">24/7 Customer Support</h3>
@@ -39,6 +42,7 @@ const Footer = () => {
         {/* Newsletter */}
         <div className="bg-blue-900 rounded-xl p-6">
           <div className="flex items-center gap-2 text-lg font-semibold mb-2">
+          <FaPaperPlane className="text-2xl" />
             <span>Newsletter</span>
           </div>
           <h3 className="text-xl font-semibold mb-1">Join our Newsletter</h3>
