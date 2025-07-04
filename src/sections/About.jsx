86b467880d049c155ddcbb0e702eedd9f49b9c7e
Your Name <you@example.com>
2025-07-04 18:57:36 +0530

    Your message

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Button from '../components/Button.jsx';
-
+import { FaHandPaper, FaThumbsUp, FaTrophy, FaDollarSign } from 'react-icons/fa';
 const WhyFunding = () => {
   const [hasCopied, setHasCopied] = useState(false);
 
@@ -19,24 +19,28 @@ const WhyFunding = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {/* Card 1 */}
           <div className="bg-[#010030] border border-blue-900 rounded-xl p-6 text-left hover:shadow-lg transition">
+           <FaHandPaper className="text-blue-500 text-3xl mb-4" />
             <p className="text-blue-400 font-semibold">Zero Reward Denials.</p>
             <p className="font-bold">Trade with a peace of mind.</p>
           </div>
 
           {/* Card 2 */}
           <div className="bg-[#010030] border border-blue-900 rounded-xl p-6 text-left hover:shadow-lg transition">
+           <FaThumbsUp className="text-blue-500 text-3xl mb-4" />
             <p className="text-blue-400 font-semibold">Your Favorite Platforms.</p>
             <p className="font-bold">MetaTrader 5, Match-Trader and cTrader.</p>
           </div>
 
           {/* Card 3 */}
           <div className="bg-[#010030] border border-blue-900 rounded-xl p-6 text-left hover:shadow-lg transition">
+          <FaTrophy className="text-blue-500 text-3xl mb-4" />
             <p className="text-blue-400 font-semibold">Flexible Reward Cycles.</p>
             <p className="font-bold">The choice is yours: weekly, bi-weekly or monthly.</p>
           </div>
 
           {/* Card 4 */}
           <div className="bg-[#010030] border border-blue-900 rounded-xl p-6 text-left hover:shadow-lg transition">
+          <FaDollarSign className="text-blue-500 text-3xl mb-4" />
             <p className="text-blue-400 font-semibold">We Grow Together.</p>
             <p className="font-bold">Trade up to $300,000 in simulated capital.</p>
           </div>
